841507aa5173813181d575ed8070741f136cd6b5
Emilio Nyaray <emilio@nyaray.com>
2018-10-28 12:03:12 +0100

    Fetch and log trivia using reqwest and serde

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
-extern crate actix_web;
 #[allow(unused)]
 #[macro_use]
 extern crate askama;
-extern crate env_logger;
 #[allow(unused)]
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate serde_derive;
+
+extern crate actix_web;
+#[allow(unused)]
+extern crate env_logger;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
 
 use actix_web::{actix, http, middleware, server, App, Error, HttpResponse, Query};
 use askama::Template;
+use reqwest::Client;
 use std::collections::HashMap;
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate<'a> {
     name: &'a str,
+}
+
+#[derive(Deserialize, Debug)]
+struct TriviaResponse {
+    response_code: u32,
+    results: Vec<TriviaSpec>,
+}
+
+#[derive(Deserialize, Debug)]
+struct TriviaSpec {
+    category: String,
+    #[serde(rename = "type")]
+    ty: String,
+    difficulty: String,
+    question: String,
+    correct_answer: String,
+    incorrect_answers: Vec<String>,
 }
 
 const HOSTNAME: &'static str = "127.0.0.1";
@@ -45,10 +70,13 @@ fn index(query: Query<HashMap<String, String>>) -> Result<HttpResponse, Error> {
     let fallback_name = &"world".to_string();
     let name = query.get("name").unwrap_or(fallback_name);
 
-    let s = IndexTemplate {
-        name: name,
-    }.render()
-    .unwrap();
+    let trivia_response: TriviaResponse = reqwest::get("https://opentdb.com/api.php?amount=1")
+        .unwrap()
+        .json()
+        .unwrap();
+    println!("{:?}", trivia_response);
+
+    let s = IndexTemplate { name: name }.render().unwrap();
 
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
