b3e7fa761ff5f21a19bdb313a9a6146aa3ace23d
Johan Burell <johan.burell@gmail.com>
2018-10-28 14:40:46 +0100

    Moved externs to a separate file to improve readability

---
+++ src/externs.rs
@@ -0,0 +1,15 @@
+#[allow(unused)]
+#[macro_use] 
+extern crate log;
+extern crate env_logger;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+#[allow(unused)]
+#[macro_use]
+extern crate askama;
+extern crate reqwest;
+extern crate actix_web;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,4 @@
-#[allow(unused)]
-#[macro_use] 
-extern crate log;
-extern crate env_logger;
-
-#[macro_use]
-extern crate serde_derive;
-extern crate serde;
-extern crate serde_json;
-
-#[allow(unused)]
-#[macro_use]
-extern crate askama;
-extern crate reqwest;
-
-extern crate actix_web;
+include!("externs.rs");
 use actix_web::actix;
 
 mod res;
